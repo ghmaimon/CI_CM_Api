8aaf78b4d491266baef5738a35317c40c6313dd1
ghmaimon <kaixokoko123@gmail.com>
2023-01-13 20:51:45 +0100

    add eureka naming server, config server, and start zones microservice

---
+++ zones-service/src/main/java/com/cicm/zonesservice/ZonesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cicm.zonesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZonesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZonesServiceApplication.class, args);
+    }
+
+}
---
+++ zones-service/src/test/java/com/cicm/zonesservice/ZonesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicm.zonesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZonesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
